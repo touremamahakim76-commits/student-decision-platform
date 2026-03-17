11002986e00ab82000e0235fa073199c985b3881
touremamahakim76-commits <touremamahakim76@gmail.com>
2026-03-17 09:29:04 +0100

    Update queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,8 +1,31 @@
--- Number of universities
-SELECT COUNT(*) FROM universite;
 
--- Average number of libraries per area
-SELECT ville, COUNT(*) FROM bibliotheque GROUP BY ville;
+-- Nombre total de logements
+SELECT COUNT(*) AS total_logements
+FROM logements;
 
--- Example analysis query
-SELECT * FROM appartement ORDER BY latitude;
+-- Nombre total de bibliothèques
+SELECT COUNT(*) AS total_bibliotheques
+FROM bibliotheques;
+
+-- Nombre total d’universités
+SELECT COUNT(*) AS total_universites
+FROM universites;
+
+-- Loyer moyen estimé
+SELECT AVG(loyer_estime) AS loyer_moyen
+FROM logements;
+
+-- Prix moyen au m²
+SELECT AVG(prix_m2) AS prix_moyen_m2
+FROM logements;
+
+-- Liste des universités
+SELECT nom
+FROM universites
+ORDER BY nom ASC;
+
+-- Top 10 logements les moins chers
+SELECT *
+FROM logements
+ORDER BY loyer_estime ASC
+LIMIT 10;
